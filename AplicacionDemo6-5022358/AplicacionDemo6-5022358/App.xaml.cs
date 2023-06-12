c69842d1165aa145f9f915f1d789f30673b80a55
kevin <Elmer@DESKTOP-VN1F8HC>
2023-06-12 12:11:40 +0200

    Agregar archivos de proyecto.

---
+++ AplicacionDemo6-5022358/AplicacionDemo6-5022358/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AplicacionDemo6_5022358
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
